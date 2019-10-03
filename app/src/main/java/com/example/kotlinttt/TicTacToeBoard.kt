5875afa66dd5c3282a210228e3f6b924bf6254b4
crkubiak <charles@8thlight.com>
2019-10-03 15:04:34 -0500

    formatted board, strings moved to @strings resource

---
+++ app/src/main/java/com/example/kotlinttt/TicTacToeBoard.kt
@@ -0,0 +1,17 @@
+package com.example.kotlinttt
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class TicTacToeBoard : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.tic_tac_toe_board, container, false)
+    }
+}
