dcba3142479dde3eae0dd3258c1443e92679f1a5
crkubiak <charles@8thlight.com>
2019-10-03 15:38:05 -0500

    fragment added to main activity

--- app/src/main/java/com/example/kotlinttt/MainActivity.kt
+++ app/src/main/java/com/example/kotlinttt/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.kotlinttt
 
 import android.os.Bundle
+import android.util.Log.d
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +17,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-
+        playButton.setOnClickListener {
+            d("charles", "play pushed")
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.frameLayout, TicTacToeBoard()).commit()
+        }
     }
 }
